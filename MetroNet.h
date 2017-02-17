1f86facff026bbb3426e13aaccb01a177e52a4f2
Andrei Bondarenko <Andrei@Gandalf-2.local>
2017-02-17 18:04:24 +0100

    MetroNet class header updated with data members and poroperlyInitialized()

--- MetroNet.h
+++ MetroNet.h
@@ -9,11 +9,32 @@
 #ifndef INC_METRONET_H
 #define INC_METRONET_H
 
+#include <map>
+#include <set>
+#include <string>
+#include "Tram.h"
+#include "Station.h"
+
 class MetroNet {
 private:
+  MetroNet* initCheck;
+  std::map<std::string, Station*> alleStations;
+  std::map<int, Station*> alleTrams;
+  std::set<int> alleSporen;
 public:
-    MetroNet();
-    ~MetroNet();
+  // CONSTRUCTOR
+  /**
+  \n ENSURE(properlyInitialized(), "constructor must end in properlyInitialized state");
+  */
+  MetroNet();
+
+  // DESTRUCTOR
+  ~MetroNet();
+
+  // INITIALIZATION CHECK
+  bool properlyInitialized();
+
+  
 protected:
 
 };
