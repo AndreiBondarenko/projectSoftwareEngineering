95b20aa37dd431294228191aa25ce96d47d58139
Andrei Bondarenko <Andrei@Gandalf-2.local>
2017-02-19 16:38:21 +0100

    Renamed inputVerwerker to BestandIO This way we don't need to make a separate class for the funtions which will be responsible for output

---
+++ BestandIO.cpp
@@ -0,0 +1,12 @@
+//
+//  BestandIO.cpp
+//  projectSoftwareEngineering
+//
+//  Created by Andrei Bondarenko on 19/02/2017.
+//
+//
+
+#include "BestandIO.h"
+
+BestandIO::BestandIO()  {}
+BestandIO::~BestandIO() {}
---
+++ BestandIO.h
@@ -0,0 +1,22 @@
+//
+//  BestandIO.h
+//  projectSoftwareEngineering
+//
+//  Created by Andrei Bondarenko on 19/02/2017.
+//
+//
+
+#ifndef BESTANDIO_H
+#define BESTANDIO_H
+
+class BestandIO {
+private:
+public:
+    BestandIO();
+    ~BestandIO();
+protected:
+
+};
+
+
+#endif /* BESTANDIO_H */
