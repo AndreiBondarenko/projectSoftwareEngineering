ff6a318ad8dd7a863df59de31705378df5f31d48
Andrei Bondarenko <Andrei@Gandalf-2.local>
2017-02-20 18:38:16 +0100

    Heb klein testje in main gedaan

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "BestandIO.h"
+#include <iostream>
 
 int main(int argc, char const *argv[]) {
-  BestandIO::initialize("_input/testInput.xml");
+  MetroNet* antwerpen = BestandIO::initialize("_input/testInput.xml");
+  std::cout << (antwerpen->getAlleStations())->size() << std::endl;
+  return (antwerpen->getAlleStations())->size();
 }
