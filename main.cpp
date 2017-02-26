b5c2513154564d1361dee0a9673ec74a6794bbf9
IgorSchittekat <igor.schittekat@gmail.com>
2017-02-26 16:14:15 +0100

    changed main to simulate with external file

--- main.cpp
+++ main.cpp
@@ -2,10 +2,23 @@
 #include <iostream>
 
 int main(int argc, char const *argv[]) {
-  MetroNet* antwerpen = MetroNet::initializeFromFile("_input/testInput.xml");
-  antwerpen->moveTrams();
-  antwerpen->moveTrams();
-  antwerpen->moveTrams();
-  antwerpen->writeToFile();
+  if (argc == 1) {
+    MetroNet* antwerpen = MetroNet::initializeFromFile("_input/testInput.xml");
+    antwerpen->moveTrams();
+    antwerpen->moveTrams();
+    antwerpen->moveTrams();
+    antwerpen->writeToFile();
+  }
+  else if (argc == 2) {
+    MetroNet* antwerpen = MetroNet::initializeFromFile(argv[1]);
+    antwerpen->moveTrams();
+    antwerpen->moveTrams();
+    antwerpen->moveTrams();
+    antwerpen->writeToFile();
+  }
+  else {
+    MetroNet* antwerpen = MetroNet::initializeFromFile(argv[1]);
+    antwerpen->simulate(argv[2]);
+  }
   return 0;
 }
