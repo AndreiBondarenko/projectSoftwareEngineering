39a9894ec0de2018ee870890c7b728df8e73d3f7
Andrei Bondarenko <Andrei@Gandalf-2.local>
2017-03-01 15:17:26 +0100

    Changed test code accordingly

--- main.cpp
+++ main.cpp
@@ -3,9 +3,6 @@
 
 int main(int argc, char const *argv[]) {
   MetroNet* antwerpen = MetroNet::initializeFromFile("_input/testInput.xml");
-  antwerpen->moveTrams();
-  antwerpen->moveTrams();
-  antwerpen->moveTrams();
-  antwerpen->writeToFile();
+  antwerpen->writeToASCII();
   return 0;
 }
