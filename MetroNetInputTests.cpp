28870f63ac7582731e052396afc29be5e8d62c48
IgorSchittekat <igor.schittekat@gmail.com>
2017-03-16 17:10:46 +0100

    added inputTest + expected testfile

--- MetroNetInputTests.cpp
+++ MetroNetInputTests.cpp
@@ -15,22 +15,43 @@ protected:
 };
 
 TEST_F(MetroNetInputTests, correctInput) {
-	ASSERT_TRUE(DirectoryExists("_tests"));
+	ASSERT_TRUE(DirectoryExists("_testOutput"));
+	ASSERT_TRUE(DirectoryExists("_testInput"));
 
 	std::ofstream ofile;
 	SuccessEnum importResult;
 
-	ofile.open("_tests/Input01.xml");
+	ofile.open("_testInput/Input01.xml");
 	ofile << "<?xml version=\"1.0\" ?>" << std::endl
-		<< "<METRONET>" << std::endl
-		<< "\t<STATION>" << std::endl
-		<< "\t\t<naam>A</naam>" << std::endl
-		<< "\t</STATION>" << std::endl
-		<< "</METRONET>" << std::endl;
+		<< "<METRONET>\n\t<STATION>\n"
+		<< "\t\t<naam>A</naam>\n\t\t<volgende>B</volgende>\n"
+		<< "<vorige>C</vorige>\n\t\t<spoor>12</spoor>\n"
+		<< "\t\t<opstappen>7</opstappen>\n"
+		<< "\t</STATION>\n\t<STATION>\n"
+		<< "\t\t<naam>B</naam>\n\t\t<volgende>C</volgende>\n"
+		<< "<vorige>A</vorige>\n\t\t<spoor>12</spoor>\n"
+		<< "\t\t<opstappen>10</opstappen>\n\t\t<afstappen>5</afstappen>\n"
+		<< "\t</STATION>\n\t<STATION>\n"
+		<< "\t\t<naam>C</naam>\n\t\t<volgende>A</volgende>\n"
+		<< "<vorige>B</vorige>\n\t\t<spoor>12</spoor>\n"
+		<< "\t</STATION>\n\t<TRAM>\n"
+		<< "\t\t<lijnNr>12</lijnNr>\n\t\t<zitplaatsen>32</zitplaatsen>\n"
+		<< "\t\t<snelheid>60</snelheid>\n\t\t<beginStation>A</beginStation>\n"
+		<< "\t</TRAM>\n</METRONET>" << std::endl;
 	ofile.close();
-	ofile.open("_tests/Error01.txt");
-	importResult = MetroNetImporter::importMetroNet("_tests/Input01.xml", ofile, metronet);
+	ofile.open("_testOutput/Error01.txt");
+	importResult = MetroNetImporter::importMetroNet("_testInput/Input01.xml", ofile, metronet);
 	ofile.close();
-	EXPECT_TRUE(importResult == PartialImport);
+	EXPECT_TRUE(importResult == Success);
 
+	ofile.open("_testOutput/test01.txt");
+	metronet.movePassengers("A", 12, ofile);
+	metronet.moveTram("B", 12, ofile);
+	metronet.movePassengers("B", 12, ofile);
+	metronet.moveTram("C", 12, ofile);
+	ofile.close();
+	EXPECT_TRUE(FileCompare("_testOutput/test01.txt", "_testOutput/test01Expected.txt"));
+	std::map<int, Tram*>* trams = metronet.getAlleTrams();
+	Tram* tram = trams->find(12)->second;
+	EXPECT_TRUE(tram->getPassagiers() == 12);
 }
--- MetroNetTests.cpp
+++ MetroNetTests.cpp
@@ -5,6 +5,7 @@
 
 #include "MetroNet.h"
 #include "MetroNetClassTests.cpp"
+#include "MetroNetInputTests.cpp"
 
 
 int main(int argc, char **argv) {
