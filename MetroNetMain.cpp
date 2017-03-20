908e808160915844d00c8c7ae8effca0a0b32054
Andrei Bondarenko <andrei.i.bondarenko@gmail.com>
2017-03-20 19:04:21 +0100

    Changed name output file and prevented it from being opened and closed twice

--- MetroNetMain.cpp
+++ MetroNetMain.cpp
@@ -6,10 +6,8 @@
 int main(int argc, char const *argv[]) {
   std::ofstream myfile, error;
   MetroNet antwerpen;
-  error.open("_output/zzzError.txt");
+  error.open("_output/MetroNetErrorLog.txt");
   MetroNetImporter::importMetroNet("_input/testInput2.xml", myfile, antwerpen);
-  error.close();
-  error.open("_output/zzzError.txt");
   myfile.open("_output/MetroNetLog.txt");
   for (int i = 0; i < 10; i++) {
   	antwerpen.moveAllePassengers(myfile, error);
