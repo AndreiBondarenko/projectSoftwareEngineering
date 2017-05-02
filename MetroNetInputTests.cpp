f2920042ce19293ea6e91747016337fe5a023998
IgorSchittekat <igor.schittekat@gmail.com>
2017-05-02 17:14:30 +0200

    added InputTests for MetroNet (files not included)

--- MetroNetInputTests.cpp
+++ MetroNetInputTests.cpp
@@ -11,276 +11,107 @@ class MetroNetInputTests: public ::testing::Test {
 protected:
 	friend class MetroNet;
 
-	MetroNet metronet;	
+	virtual void SetUp() {
+		metronet = new MetroNet();
+	}
+
+	MetroNet* metronet;	
 };
-/*
+
+TEST_F(MetroNetInputTests, InputHappyDay) {
+	ASSERT_TRUE(DirectoryExists("_testInput"));
+
+	std::ofstream ofile;
+	SuccessEnum importResult;
+
+	ofile.open("_testInput/correctInput.xml");
+	ofile << "<?xml version=\"1.0\" ?>" << std::endl
+		<< "<METRONET>\n\t<STATION>\n"
+		<< "\t\t<naam>A</naam>\n\t\t<type>Metrostation</type>\n"
+		<< "\t\t<SPOOR>\n\t\t\t<spoor>12</spoor>\n"
+		<< "\t\t\t<volgende>B</volgende>\n\t\t\t<vorige>B</vorige>\n"
+		<< "\t\t</SPOOR>\n\t</STATION>\n\t<STATION>\n"
+		<< "\t\t<naam>B</naam>\n\t\t<type>Halte</type>\n"
+		<< "\t\t<SPOOR>\n\t\t\t<spoor>12</spoor>\n"
+		<< "\t\t\t<volgende>A</volgende>\n\t\t\t<vorige>A</vorige>\n"
+		<< "\t\t</SPOOR>\n\t</STATION>\n\t<TRAM>\n"
+		<< "\t\t<lijnNr>12</lijnNr>\n\t\t<zitplaatsen>32</zitplaatsen>\n"
+		<< "\t\t<snelheid>60</snelheid>\n\t\t<beginStation>A</beginStation>\n"
+		<< "\t\t<type>PCC</type>\n\t\t<voertuigNr>1</voertuigNr>\n"
+		<< "\t</TRAM>\n</METRONET>" << std::endl;
+	ofile.close();
+	ofile.open("_testInput/zzzError.txt");
+	importResult = MetroNetImporter::importMetroNet("_testInput/correctInput.xml", ofile, *metronet);
+	ofile.close();
+	EXPECT_TRUE(importResult == Success);
+	EXPECT_TRUE(FileIsEmpty("_testInput/zzzError.txt"));
+}
+
 TEST_F(MetroNetInputTests, correctInput) {
-	ASSERT_TRUE(DirectoryExists("_testOutput"));
 	ASSERT_TRUE(DirectoryExists("_testInput"));
-	ASSERT_TRUE(DirectoryExists("_testOutput/expected"));
-
-	std::ofstream ofile, error;
-	SuccessEnum importResult;
-
-	ofile.open("_testInput/Input01.xml");
-	ofile << "<?xml version=\"1.0\" ?>" << std::endl
-		<< "<METRONET>\n\t<STATION>\n"
-		<< "\t\t<naam>A</naam>\n\t\t<volgende>B</volgende>\n"
-		<< "<vorige>C</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t\t<opstappen>7</opstappen>\n"
-		<< "\t</STATION>\n\t<STATION>\n"
-		<< "\t\t<naam>B</naam>\n\t\t<volgende>C</volgende>\n"
-		<< "<vorige>A</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t\t<opstappen>10</opstappen>\n\t\t<afstappen>5</afstappen>\n"
-		<< "\t</STATION>\n\t<STATION>\n"
-		<< "\t\t<naam>C</naam>\n\t\t<volgende>A</volgende>\n"
-		<< "<vorige>B</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t</STATION>\n\t<TRAM>\n"
-		<< "\t\t<lijnNr>12</lijnNr>\n\t\t<zitplaatsen>32</zitplaatsen>\n"
-		<< "\t\t<snelheid>60</snelheid>\n\t\t<beginStation>A</beginStation>\n"
-		<< "\t</TRAM>\n</METRONET>" << std::endl;
-	ofile.close();
-	ofile.open("_testOutput/Error01.txt");
-	importResult = MetroNetImporter::importMetroNet("_testInput/Input01.xml", ofile, metronet);
-	ofile.close();
-	EXPECT_TRUE(importResult == Success);
-
-	ofile.open("_testOutput/test01.txt");
-	error.open("_testOutput//Error01.txt");
-	metronet.movePassengers("A", 12, ofile, error);
-	metronet.moveTram("B", 12, ofile);
-	metronet.movePassengers("B", 12, ofile, error);
-	metronet.moveTram("C", 12, ofile);
-	ofile.close();
-	error.close();
-	EXPECT_TRUE(FileCompare("_testOutput/test01.txt", "_testOutput/expected/test01Expected.txt"));
-	std::map<int, Tram*>* trams = metronet.getAlleTrams();
-	Tram* tram = trams->find(12)->second;
-	EXPECT_TRUE(tram->getPassagiers() == 12);
-	EXPECT_TRUE(tram->getCurrentStation() == "C");
-}
-
-TEST_F(MetroNetInputTests, correctInput2) {
-	ASSERT_TRUE(DirectoryExists("_testOutput"));
-	ASSERT_TRUE(DirectoryExists("_testInput"));
-	ASSERT_TRUE(DirectoryExists("_testOutput/expected"));
-
-	std::ofstream ofile, error;
-	SuccessEnum importResult;
-
-	ofile.open("_testInput/Input07.xml");
-	ofile << "<?xml version=\"1.0\" ?>" << std::endl
-		<< "<METRONET>\n\t<STATION>\n"
-		<< "\t\t<naam>A</naam>\n\t\t<volgende>B</volgende>\n"
-		<< "<vorige>C</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t\t<opstappen>7</opstappen>\n"
-		<< "\t</STATION>\n\t<STATION>\n"
-		<< "\t\t<naam>B</naam>\n\t\t<volgende>C</volgende>\n"
-		<< "<vorige>A</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t\t<opstappen>10</opstappen>\n\t\t<afstappen>5</afstappen>\n"
-		<< "\t</STATION>\n\t<STATION>\n"
-		<< "\t\t<naam>C</naam>\n\t\t<volgende>A</volgende>\n"
-		<< "<vorige>B</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t</STATION>\n\t<TRAM>\n"
-		<< "\t\t<lijnNr>12</lijnNr>\n\t\t<zitplaatsen>32</zitplaatsen>\n"
-		<< "\t\t<snelheid>60</snelheid>\n\t\t<beginStation>A</beginStation>\n"
-		<< "\t</TRAM>\n</METRONET>" << std::endl;
-	ofile.close();
-	ofile.open("_testOutput/Error07.txt");
-	importResult = MetroNetImporter::importMetroNet("_testInput/Input07.xml", ofile, metronet);
-	ofile.close();
-	EXPECT_TRUE(importResult == Success);
-
-	ofile.open("_testOutput/test07.txt");
-	error.open("_testOutput//Error07.txt");
-	metronet.moveAllePassengers(ofile, error);
-	metronet.moveAlleTrams(ofile);
-	metronet.moveAllePassengers(ofile, error);
-	metronet.moveAlleTrams(ofile);
-	ofile.close();
-	error.close();
-	EXPECT_TRUE(FileCompare("_testOutput/test07.txt", "_testOutput/expected/test07Expected.txt"));
-	std::map<int, Tram*>* trams = metronet.getAlleTrams();
-	Tram* tram = trams->find(12)->second;
-	EXPECT_TRUE(tram->getPassagiers() == 12);
-	EXPECT_TRUE(tram->getCurrentStation() == "C");
-}
-
-// error : MERONET ipv METRONET
-TEST_F(MetroNetInputTests, wrongSyntax) {
-	ASSERT_TRUE(DirectoryExists("_testOutput"));
-	ASSERT_TRUE(DirectoryExists("_testInput"));
-	ASSERT_TRUE(DirectoryExists("_testOutput/expected"));
 
 	std::ofstream ofile;
 	SuccessEnum importResult;
+	int fileCounter = 1;
+	std::string fileName = "_testInput/correctInput" + std::to_string(fileCounter) + ".xml";
 
-	ofile.open("_testInput/Input02.xml");
-	ofile << "<?xml version=\"1.0\" ?>" << std::endl
-		<< "<MERONET>\n\t<STATION>\n"
-		<< "\t\t<naam>A</naam>\n\t\t<volgende>B</volgende>\n"
-		<< "\t\t<vorige>C</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t\t<opstappen>7</opstappen>\n"
-		<< "\t</STATION>\n\t<STATION>\n"
-		<< "\t\t<naam>B</naam>\n\t\t<volgende>C</volgende>\n"
-		<< "\t\t<vorige>A</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t\t<opstappen>10</opstappen>\n\t\t<afstappen>5</afstappen>\n"
-		<< "\t</STATION>\n\t<STATION>\n"
-		<< "\t\t<naam>C</naam>\n\t\t<volgende>A</volgende>\n"
-		<< "\t\t<vorige>B</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t</STATION>\n\t<TRAM>\n"
-		<< "\t\t<lijnNr>12</lijnNr>\n\t\t<zitplaatsen>32</zitplaatsen>\n"
-		<< "\t\t<snelheid>60</snelheid>\n\t\t<beginStation>A</beginStation>\n"
-		<< "\t</TRAM>\n</METRONET>" << std::endl;
-	ofile.close();
-	ofile.open("_testOutput/Error02.txt");
-	importResult = MetroNetImporter::importMetroNet("_testInput/Input02.xml", ofile, metronet);
-	ofile.close();
-	EXPECT_TRUE(importResult == ImportAborted);
-	EXPECT_TRUE(FileCompare("_testOutput/Error02.txt", "_testOutput/expected/Error02Expected.txt"));
+	while (FileExists(fileName)) {
+		ofile.open("_testInput/zzzError.txt");
+		importResult = MetroNetImporter::importMetroNet(fileName.c_str(), ofile, *metronet);
+		ofile.close();
+		EXPECT_TRUE(importResult == Success);
+		EXPECT_TRUE(FileIsEmpty("_testInput/zzzError.txt"));
+
+		fileCounter++;
+		fileName = "_testInput/correctInput" + std::to_string(fileCounter) + ".xml";
+	}
+	EXPECT_TRUE(fileCounter == 2);
 }
 
-// error : name is missing in station A, vorige is missing in station B
-TEST_F(MetroNetInputTests, invalidInput) {
-	ASSERT_TRUE(DirectoryExists("_testOutput"));
+TEST_F(MetroNetInputTests, wrongInputSyntaxError) {
 	ASSERT_TRUE(DirectoryExists("_testInput"));
-	ASSERT_TRUE(DirectoryExists("_testOutput/expected"));
-
-	std::ofstream ofile;
-
-	ofile.open("_testInput/Input03.xml");
-	ofile << "<?xml version=\"1.0\" ?>" << std::endl
-		<< "<METRONET>\n\t<STATION>\n"
-		<< "\t\t<volgende>B</volgende>\n"
-		<< "\t\t<vorige>C</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t\t<opstappen>7</opstappen>\n"
-		<< "\t</STATION>\n\t<STATION>\n"
-		<< "\t\t<naam>B</naam>\n\t\t<volgende>C</volgende>\n"
-		<< "\t\t<spoor>12</spoor>\n"
-		<< "\t\t<opstappen>10</opstappen>\n\t\t<afstappen>5</afstappen>\n"
-		<< "\t</STATION>\n\t<STATION>\n"
-		<< "\t\t<naam>C</naam>\n\t\t<volgende>A</volgende>\n"
-		<< "\t\t<vorige>B</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t</STATION>\n\t<TRAM>\n"
-		<< "\t\t<lijnNr>12</lijnNr>\n\t\t<zitplaatsen>32</zitplaatsen>\n"
-		<< "\t\t<snelheid>60</snelheid>\n\t\t<beginStation>A</beginStation>\n"
-		<< "\t</TRAM>\n</METRONET>" << std::endl;
-	ofile.close();
-	ofile.open("_testOutput/Error03.txt");
-	EXPECT_DEATH(MetroNetImporter::importMetroNet("_testInput/Input03.xml", ofile, metronet), 
-		"MetroNet is inconsistent");
-	ofile.close();
-	EXPECT_TRUE(FileCompare("_testOutput/Error03.txt", "_testOutput/expected/Error03Expected.txt"));
-}
-
-// error : spoor missing in station D, still consistent metronet
-TEST_F(MetroNetInputTests, invalidInput2) {
-	ASSERT_TRUE(DirectoryExists("_testOutput"));
-	ASSERT_TRUE(DirectoryExists("_testInput"));
-	ASSERT_TRUE(DirectoryExists("_testOutput/expected"));
+	ASSERT_TRUE(DirectoryExists("_testInput/expected"));
 
 	std::ofstream ofile;
 	SuccessEnum importResult;
+	int fileCounter = 1;
+	std::string fileName = "_testInput/syntaxErrorInput" + std::to_string(fileCounter) + ".xml";
 
-	ofile.open("_testInput/Input04.xml");
-	ofile << "<?xml version=\"1.0\" ?>" << std::endl
-		<< "<METRONET>\n\t<STATION>\n"
-		<< "\t\t<naam>A</naam>\n\t\t<volgende>B</volgende>\n"
-		<< "\t\t<vorige>C</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t\t<opstappen>7</opstappen>\n"
-		<< "\t</STATION>\n\t<STATION>\n"
-		<< "\t\t<naam>B</naam>\n\t\t<volgende>C</volgende>\n"
-		<< "\t\t<vorige>A</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t\t<opstappen>10</opstappen>\n\t\t<afstappen>5</afstappen>\n"
-		<< "\t</STATION>\n\t<STATION>\n"
-		<< "\t\t<naam>C</naam>\n\t\t<volgende>A</volgende>\n"
-		<< "\t\t<vorige>B</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t</STATION>\n\t<STATION>\n"
-		<< "\t\t<naam>D</naam>\n\t\t<volgende>E</volgende>\n"
-		<< "\t\t<vorige>F</vorige>\n"
-		<< "\t</STATION>\n\t<TRAM>\n"
-		<< "\t\t<lijnNr>12</lijnNr>\n\t\t<zitplaatsen>32</zitplaatsen>\n"
-		<< "\t\t<snelheid>60</snelheid>\n\t\t<beginStation>A</beginStation>\n"
-		<< "\t</TRAM>\n</METRONET>" << std::endl;
-	ofile.close();
-	ofile.open("_testOutput/Error04.txt");
-	importResult = MetroNetImporter::importMetroNet("_testInput/Input04.xml", ofile, metronet);
-	ofile.close();
-	EXPECT_TRUE(importResult == PartialImport);
-	EXPECT_TRUE(FileCompare("_testOutput/Error04.txt", "_testOutput/expected/Error04Expected.txt"));
+	while (FileExists(fileName)) {
+		ofile.open("_testInput/zzzError.txt");
+		importResult = MetroNetImporter::importMetroNet(fileName.c_str(), ofile, *metronet);
+		ofile.close();
+		EXPECT_TRUE(importResult == ImportAborted);
+		std::string expectedFileName = "_testInput/expected/syntaxError" + std::to_string(fileCounter) + ".txt";
+		EXPECT_TRUE(FileCompare("_testInput/zzzError.txt", expectedFileName));
+
+		fileCounter++;
+		fileName = "_testInput/syntaxErrorInput" + std::to_string(fileCounter) + ".xml";
+	}
+	EXPECT_TRUE(fileCounter == 6);
 }
 
-// error : Tram 13 missing zitplaatsen
-TEST_F(MetroNetInputTests, invalidInput3) {
-	ASSERT_TRUE(DirectoryExists("_testOutput"));
+TEST_F(MetroNetInputTests, wrongInput) {
 	ASSERT_TRUE(DirectoryExists("_testInput"));
-	ASSERT_TRUE(DirectoryExists("_testOutput/expected"));
+	ASSERT_TRUE(DirectoryExists("_testInput/expected"));
 
 	std::ofstream ofile;
 	SuccessEnum importResult;
+	int fileCounter = 1;
+	std::string fileName = "_testInput/wrongInput" + std::to_string(fileCounter) + ".xml";
 
-	ofile.open("_testInput/Input05.xml");
-	ofile << "<?xml version=\"1.0\" ?>" << std::endl
-		<< "<METRONET>\n\t<STATION>\n"
-		<< "\t\t<naam>A</naam>\n\t\t<volgende>B</volgende>\n"
-		<< "\t\t<vorige>C</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t\t<opstappen>7</opstappen>\n"
-		<< "\t</STATION>\n\t<STATION>\n"
-		<< "\t\t<naam>B</naam>\n\t\t<volgende>C</volgende>\n"
-		<< "\t\t<vorige>A</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t\t<opstappen>10</opstappen>\n\t\t<afstappen>5</afstappen>\n"
-		<< "\t</STATION>\n\t<STATION>\n"
-		<< "\t\t<naam>C</naam>\n\t\t<volgende>A</volgende>\n"
-		<< "\t\t<vorige>B</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t</STATION>\n\t<TRAM>\n"
-		<< "\t\t<lijnNr>12</lijnNr>\n\t\t<zitplaatsen>32</zitplaatsen>\n"
-		<< "\t\t<snelheid>60</snelheid>\n\t\t<beginStation>A</beginStation>\n"
-		<< "\t</TRAM>\n\t<TRAM>\n"
-		<< "\t\t<lijnNr>13</lijnNr>\n"
-		<< "\t\t<snelheid>60</snelheid>\n\t\t<beginStation>A</beginStation>\n"
-		<< "\t</TRAM>\n</METRONET>" << std::endl;
-	ofile.close();
-	ofile.open("_testOutput/Error05.txt");
-	importResult = MetroNetImporter::importMetroNet("_testInput/Input05.xml", ofile, metronet);
-	ofile.close();
-	EXPECT_TRUE(importResult == PartialImport);
-	EXPECT_TRUE(FileCompare("_testOutput/Error05.txt", "_testOutput/expected/Error05Expected.txt"));
+	while (FileExists(fileName)) {
+		SetUp();
+		ofile.open("_testInput/zzzError.txt");
+		importResult = MetroNetImporter::importMetroNet(fileName.c_str(), ofile, *metronet);
+		ofile.close();
+		EXPECT_TRUE(importResult == PartialImport);
+		std::string expectedFileName = "_testInput/expected/wrongInput" + std::to_string(fileCounter) + ".txt";
+		EXPECT_TRUE(FileCompare("_testInput/zzzError.txt", expectedFileName));
+
+		fileCounter++;
+		fileName = "_testInput/wrongInput" + std::to_string(fileCounter) + ".xml";
+	}
+	// EXPECT_TRUE(fileCounter == 6);
 }
 
-// error : Tram 13 missing negative snelheid
-TEST_F(MetroNetInputTests, invalidInput4) {
-	ASSERT_TRUE(DirectoryExists("_testOutput"));
-	ASSERT_TRUE(DirectoryExists("_testInput"));
-	ASSERT_TRUE(DirectoryExists("_testOutput/expected"));
-
-	std::ofstream ofile;
-	SuccessEnum importResult;
-
-	ofile.open("_testInput/Input06.xml");
-	ofile << "<?xml version=\"1.0\" ?>" << std::endl
-		<< "<METRONET>\n\t<STATION>\n"
-		<< "\t\t<naam>A</naam>\n\t\t<volgende>B</volgende>\n"
-		<< "\t\t<vorige>C</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t\t<opstappen>7</opstappen>\n"
-		<< "\t</STATION>\n\t<STATION>\n"
-		<< "\t\t<naam>B</naam>\n\t\t<volgende>C</volgende>\n"
-		<< "\t\t<vorige>A</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t\t<opstappen>10</opstappen>\n\t\t<afstappen>5</afstappen>\n"
-		<< "\t</STATION>\n\t<STATION>\n"
-		<< "\t\t<naam>C</naam>\n\t\t<volgende>A</volgende>\n"
-		<< "\t\t<vorige>B</vorige>\n\t\t<spoor>12</spoor>\n"
-		<< "\t</STATION>\n\t<TRAM>\n"
-		<< "\t\t<lijnNr>12</lijnNr>\n\t\t<zitplaatsen>32</zitplaatsen>\n"
-		<< "\t\t<snelheid>60</snelheid>\n\t\t<beginStation>A</beginStation>\n"
-		<< "\t</TRAM>\n\t<TRAM>\n"
-		<< "\t\t<lijnNr>13</lijnNr>\n\t\t<zitplaatsen>32</zitplaatsen>\n"
-		<< "\t\t<snelheid>-60</snelheid>\n\t\t<beginStation>A</beginStation>\n"
-		<< "\t</TRAM>\n</METRONET>" << std::endl;
-	ofile.close();
-	ofile.open("_testOutput/Error06.txt");
-	importResult = MetroNetImporter::importMetroNet("_testInput/Input06.xml", ofile, metronet);
-	ofile.close();
-	EXPECT_TRUE(importResult == PartialImport);
-	EXPECT_TRUE(FileCompare("_testOutput/Error06.txt", "_testOutput/expected/Error06Expected.txt"));
-}
-*/
